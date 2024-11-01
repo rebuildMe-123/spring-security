8c5d1ad45f5c9300dc2f4815a6a9520560faa6b9
kumar gaurav <kumargaurav@kumars-MacBook-Air.local>
2024-11-01 19:48:08 +0530

    Initial commit

---
+++ src/test/java/com/rebuild/java_program/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebuild.java_program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
